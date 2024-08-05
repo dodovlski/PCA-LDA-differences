4e95aa315bff0e24c78210679e220a33dac8066e
dodovlski <dogukantt27@gmail.com>
2024-08-05 15:51:52 +0300

    LDA algo added

--- pca.py
+++ pca.py
@@ -1,26 +1,19 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Jul  8 10:03:40 2018
 
-@author: sadievrenseker
-"""
-
-#1. kutuphaneler
+#1. Libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# veri kümesi
+# DataSet
 veriler = pd.read_csv('Wine.csv')
 X = veriler.iloc[:, 0:13].values
 y = veriler.iloc[:, 13].values
 
-# eğitim ve test kümelerinin bölünmesi
+# Splitting Training and Test Sets
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
-# Ölçekleme
+# Scaling
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
@@ -33,35 +26,72 @@ pca = PCA(n_components = 2)
 X_train2 = pca.fit_transform(X_train)
 X_test2 = pca.transform(X_test)
 
-#pca dönüşümünden önce gelen LR
+# LR Before PCA Transformation
 from sklearn.linear_model import LogisticRegression
 classifier = LogisticRegression(random_state=0)
 classifier.fit(X_train,y_train)
 
-#pca dönüşümünden sonra gelen LR
+# LR After PCA Transformation
 classifier2 = LogisticRegression(random_state=0)
 classifier2.fit(X_train2,y_train)
 
-#tahminler
+# Predicts
 y_pred = classifier.predict(X_test)
 
 y_pred2 = classifier2.predict(X_test2)
 
 from sklearn.metrics import confusion_matrix
-#actual / PCA olmadan çıkan sonuç
+# Actual / Result without PCA 
 print('gercek / PCAsiz')
 cm = confusion_matrix(y_test,y_pred)
 print(cm)
 
-#actual / PCA sonrası çıkan sonuç
+# Actual / Result after PCA
 print("gercek / pca ile")
 cm2 = confusion_matrix(y_test,y_pred2)
 print(cm2)
 
-#PCA sonrası / PCA öncesi
+# After PCA / Before PCA
 print('pcasiz ve pcali')
 cm3 = confusion_matrix(y_pred,y_pred2)
 print(cm3)
+
+# LDA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+lda = LDA(n_components=2)
+
+X_train_lda = lda.fit_transform(X_train, y_train)
+X_test_lda = lda.transform(X_test)
+
+# LR After LDA Transformation
+classifier_lda = LogisticRegression(random_state=0)
+classifier_lda.fit(X_train_lda,y_train)
+
+# LDA Predict
+y_pred_lda = classifier_lda.predict(X_test_lda)
+
+
+# After LDA / Before LDA
+print('ldasiz ve ldali')
+cm4 = confusion_matrix(y_pred_lda,y_pred)
+print(cm4)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
